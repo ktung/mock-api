b1e07fb0f102b566bdd14749b24230b212751c71
ktung <ptung@protonmail.ch>
2017-07-02 13:53:32 +0200

    Print logs

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 Java Servlet Mock API GET/POST
 
 [Link](https://mockrest.herokuapp.com/v1)
+
+## Heroku Deploy
+
+[Doc](https://devcenter.heroku.com/articles/deploying-java-applications-with-the-heroku-maven-plugin)
+
+```
+mvn clean heroku:deploy-war
+```
--- src/main/java/com/ktung/mockrest/Api.java
+++ src/main/java/com/ktung/mockrest/Api.java
@@ -2,6 +2,7 @@ package com.ktung.mockrest;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -27,8 +28,11 @@ public class Api extends HttpServlet {
 
 	private static Logger LOGGER = LoggerFactory.getLogger(Api.class);
 
-	private int maxFileSize = 10 * 1024;
-	private int maxMemSize = 5 * 1024;
+	private static final String GET_REQ  = "GET Request";
+	private static final String POST_REQ = "POST Request";
+
+	private int maxFileSize = 10_000 * 1024; // 10 mo
+	private int maxMemSize  = 5 * 1024; // 5 ko
 
 	@Override
 	public void init() throws ServletException {
@@ -37,15 +41,18 @@ public class Api extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		LOGGER.info("GET Request");
+		PrintWriter out = resp.getWriter();
+		out.println(GET_REQ);
+		LOGGER.info(GET_REQ);
 
-		logParameters(req);
+		printParameters(req, out);
+		out.close();
 
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		LOGGER.info("POST Request");
+		LOGGER.info(POST_REQ);
 
 		boolean isMultipart = ServletFileUpload.isMultipartContent(req);
 		LOGGER.info(String.format("Mutipart request %s", isMultipart));
@@ -62,60 +69,48 @@ public class Api extends HttpServlet {
 			// maximum file size to be uploaded.
 			upload.setSizeMax(maxFileSize);
 
-			try {
+			try (PrintWriter out = resp.getWriter()) {
 				List<FileItem> fileItems = upload.parseRequest(req);
 				Iterator<FileItem> it = fileItems.iterator();
 
 				while (it.hasNext()) {
 					FileItem item = it.next();
-					if (!item.isFormField()) {
-						LOGGER.info(String.format("file { fieldName = %s, filename = %s }", item.getFieldName(),
-						        item.getName()));
-					} else {
-						LOGGER.info(String.format("param { fieldName = %s, value = %s }", item.getFieldName(),
-						        item.getString()));
 
-					}
+					String paramLog = (!item.isFormField())
+						? String.format("file { fieldName = %s, filename = %s, content = %s }"
+							, item.getFieldName(), item.getName(), item.getString())
+						: String.format("param { fieldName = %s, value = %s }"
+							, item.getFieldName(), item.getString());
+
+					out.println(paramLog);
+					LOGGER.info(paramLog);
 				}
 			} catch (FileUploadException e) {
 				e.printStackTrace();
 				LOGGER.error("Parsing file items : ", e);
 			}
 		} else {
-			logParameters(req);
+			PrintWriter out = resp.getWriter();
+			printParameters(req, out);
+			out.close();
 		}
 	}
 
-	private void logParameters(HttpServletRequest req) {
+	private void printParameters(HttpServletRequest req, PrintWriter writer) {
 		Map<String, String[]> params = req.getParameterMap();
 
 		params.forEach((k, v) -> {
-			String paramLog = "param { ";
-			paramLog += String.format("%s = ", k);
+			StringBuilder paramLog = new StringBuilder("param { ");
+
+			paramLog.append(String.format("%s = ", k));
 			for (String val : v) {
-				paramLog += String.format("%s ", val);
+				paramLog.append(String.format("%s)", val));
 			}
 
-			paramLog += " }";
-			LOGGER.info(paramLog);
+			paramLog.append(" }");
+			writer.println(paramLog.toString());
+			LOGGER.info(paramLog.toString());
 		});
-
-//		String paramsLog = "parameters { ";
-//		Iterator<Entry<String, String[]>> it = params.entrySet().iterator();
-//		while (it.hasNext()) {
-//			Map.Entry<String, String[]> entry = it.next();
-//			String key = entry.getKey();
-//			String[] values = entry.getValue();
-//
-//			paramsLog += String.format("%s = ", key);
-//			for (String val : values) {
-//				paramsLog += String.format("%s ", val);
-//			}
-//
-//			paramsLog += ", ";
-//		}
-//		paramsLog += " }";
-//		LOGGER.info(paramsLog);
 	}
 
 }
